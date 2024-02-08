04b41a66ca3e360b9af8f355a5c26fa2ad598024
sabin <sabin.pla@gmail.com>
2024-02-07 23:33:01 -0500

    added css styling and more event handlers

--- README.md
+++ README.md
@@ -9,10 +9,6 @@ Currently is just a window that will hide and unhide when the program is called.
 The idea is to bind it to the super key and use it as an applications launcher/control-center-panel-type thing. 
 
 ## Ordered todo:
-- Show window layer in all work spaces
-- Spike: See if I can move the launcher to another monitor with directional actions (and refocus a window in that monitor)　in Hyprland
-- Spike: Set up listeners to left/right arrow keys (will be used to filter query type)
-- Show time
 - Workspace Indicators
 - Run applications (.Desktop)
 	-	Launcher entries have copy-able File Path (does not trigger quit accelerator)
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use std::os::raw::c_char;
 use std::ffi::CString;
 use std::fs::File;
 use gtk::PropagationPhase;
-
+use std::time::{Duration, SystemTime};
 
 static  mut launcher: Launcher = Launcher { 
 	state: State::NotStarted, 
@@ -27,6 +27,12 @@ static  mut launcher: Launcher = Launcher {
 
 thread_local! {
     static WINDOW: RefCell<Option<gtk::ApplicationWindow>> = RefCell::new(None);
+}
+
+fn get_time_str() -> String {
+    let date_time  =  chrono::offset::Local::now();
+    let formatted = format!("{}", date_time.format("%d/%m/%Y %H:%M"));
+    formatted
 }
 
 fn key_handler(ec: &gtk::EventControllerKey, 
@@ -43,7 +49,15 @@ fn key_handler(ec: &gtk::EventControllerKey,
             )
         }
     }
-    gtk::glib::Propagation::Stop
+    gtk::glib::Propagation::Proceed
+}
+
+fn handle_mouse_click(_: &gtk::GestureClick, _n_press: i32, _x: f64,  _y: f64) {
+    println!("click  {_n_press} {_x} {_y}");
+}
+
+fn listbox_hover_handler(_: &gtk::EventControllerMotion, _x: f64, _y: f64) {
+    println!("hover  {_x} {_y}");
 }
 
 unsafe fn activate(application: &gtk::Application) {
@@ -74,15 +88,31 @@ unsafe fn activate(application: &gtk::Application) {
 
 unsafe fn startup(application: &gtk::Application) {
     println!("Starting up...");
+
     let w = gtk::ApplicationWindow::new(application);
     let action_close = gio::ActionEntry::builder("close")
         .activate(|w: & gtk::ApplicationWindow, _, _| {
             w.close();
         })
         .build();
+
+    let provider = gtk::CssProvider::new();
+    let mut css_path = glib::user_config_dir();
+    css_path.push("generic_launcher/launcher.css");
+    provider.load_from_path(css_path.as_path());
+
+    gtk::style_context_add_provider_for_display(
+        &gdk::Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
+
     w.add_action_entries([action_close]);
     let input_field = gtk::Entry::new();
     w.init_layer_shell();
+
+    // consider https://gtk-rs.org/gtk4-rs/git/docs/gdk4/prelude/trait.SurfaceExt.html connect_leave_monitor workaround
+    // also note TopLevelExt  focus(&self, timestamp: u32
+    //  inhibit_system_shortcuts for league wrapper
     w.set_layer(Layer::Overlay);
     w.auto_exclusive_zone_enable();
     w.set_margin(Edge::Left, 90);
@@ -99,11 +129,50 @@ unsafe fn startup(application: &gtk::Application) {
     for (anchor, state) in anchors {
         w.set_anchor(anchor, state);
     }
+    let root = gtk::Box::new(gtk::Orientation::Vertical, 5);
+    let result_box = gtk::Box::new(gtk::Orientation::Vertical, 5);
+
+    let mut result_frames: Vec<gtk::Frame> = Vec::new();
+
+    let mouse_controller = gtk::EventControllerMotion::new();
+    mouse_controller.connect_motion(listbox_hover_handler);
+    // mouse_controller.connect_stylus_only_notify(listbox_hover_handler_new);
+    result_box.add_controller(mouse_controller);
+
+    for i in 0..5 {
+        let label = format!("label {}", i);
+        let frame = gtk::Frame::new(Some(&label));
+        let gcc_right_click = gtk::GestureClick::builder()
+            .button(1).propagation_phase(PropagationPhase::Capture).build();
+        gcc_right_click.connect_pressed(handle_mouse_click);
+        frame.add_controller(gcc_right_click);
+        result_frames.push(frame);
+    }
+
+    for f in result_frames  {
+        result_box.append(&f);
+    }
+
     let ec = gtk::EventControllerKey::builder()
         .propagation_phase(PropagationPhase::Capture).build();
     ec.connect_key_pressed(key_handler);    //ec.forward(&input_field);
     input_field.add_controller(ec);
-    w.set_child(Some(&input_field));
+
+    let clock = gtk::Label::default();
+    clock.set_text(&get_time_str()  );
+    root.append(&clock);
+
+    let tick = move || { 
+        clock.set_text(&get_time_str());
+        glib::ControlFlow::Continue
+    };
+
+    // executes the closure once every second
+    glib::timeout_add_seconds_local(1, tick);
+    
+    root.append(&input_field);
+    root.append(&result_box);
+    w.set_child(Some(&root));
     input_field.grab_focus_without_selecting();
     w.set_keyboard_mode(KeyboardMode::Exclusive);
     w.show();
